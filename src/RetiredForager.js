074a7e312a28a6c212e89d5da5a0ab940c5bf07a
miriam30 <miriam.alvarez.p96@gmail.com>
2018-01-15 19:35:31 -0600

    finished retiredForager

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,15 @@
 var RetiredForagerBee = function() {
-};
+  ForagerBee.call(this);
+  this.age = 40;
+  this.job = "gamble";
+  this.canFly = false;
+  this.color = "grey";
 
+  RetiredForagerBee.prototype.forage = function("I am too old, let me play cards instead"){
+  };
+  RetiredForagerBee.prototype.gamble = function(treasure){
+    this.treasureChest.push(treasure)
+  };
+};
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+RetiredForagerBee.prototype.constructor = RetiredForagerBee
